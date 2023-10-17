dc5f815f5d96796be7557cc9d97a73a98cb7cc3e
Aitken Bolat <aitken.bolat@gmail.com>
2023-10-17 14:23:30 +0600

    feature:ExchangeRateService

--- src/main/java/Service/ExchangeRateService.java
+++ src/main/java/Service/ExchangeRateService.java
@@ -2,28 +2,50 @@ package Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.extern.log4j.Log4j2;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+@Log4j2
 public class ExchangeRateService extends SQLConnection{
 
     ObjectMapper objectMapper = new ObjectMapper();
 
 
-    public void getExchangeRates(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+    public void getExchangeRates(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
 
         ArrayNode arrayNode = objectMapper.createArrayNode();
+        PrintWriter pw = response.getWriter();
 
-        Statement statement = getConnection().createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM exchangerates");
+        try {
+
+            Statement statement = getConnection().createStatement();
+            ResultSet rs = statement.executeQuery("SELECT c.*,er.rate FROM Currencies c INNER JOIN exchangerates er" +
+                    " ON c.id = er.basecurrencyid");
 
 
-        while (resultSet.next()){
-
+            while (rs.next()) {
+                ObjectNode rowObject = objectMapper.createObjectNode();
+                rowObject.put("id", rs.getInt("id"));
+                rowObject.put("code", rs.getString("code"));
+                rowObject.put("fullname", rs.getString("fullname"));
+                rowObject.put("sign", rs.getString("sign"));
+                rowObject.put("rate", rs.getDouble("rate"));
+                arrayNode.add(rowObject);
+            }
+            response.setStatus(200);
+            pw.print(arrayNode.toString());
+            statement.close();
+            getConnection().close();
+        }catch (SQLException e){
+            log.error(e.getMessage());
+            response.setStatus(500);
         }
     }
 }
--- src/main/java/Servlets/ExchangeRateServlet.java
+++ src/main/java/Servlets/ExchangeRateServlet.java
@@ -1,15 +1,23 @@
 package Servlets;
 
+import Service.ExchangeRateService;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "ExchangeRateServlet", value = "/ExchangeRateServlet")
 public class ExchangeRateServlet extends HttpServlet {
+    private ExchangeRateService service = new ExchangeRateService();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        try {
+            service.getExchangeRates(request,response);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
